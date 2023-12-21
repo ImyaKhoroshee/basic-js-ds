5685b8aebabc7df6a4a5939d23014c6afaf3f62c
tatyana <tilkka@mail.ru>
2023-12-21 10:58:47 +0300

    feat: queue is finished

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+//  FIFO - First In First Out
+
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
+  getUnderlyingList() {
+    return this.head;
+  }
+  isEmpty() {
+    return this.head === null;
+  }
+  enqueue(value) {
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const node = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size += 1;
+    return this;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isEmpty()) return null;
+    const elementToDelete = this.head;
+
+    if (this.head === this.tail) {
+      this.tail = null;
+      this.head = null;
+      this.size -= 1;
+      return elementToDelete.value;
+    }
+    this.head = this.head.next;
+    this.size -= 1;
+    return elementToDelete.value;
   }
 }
 
